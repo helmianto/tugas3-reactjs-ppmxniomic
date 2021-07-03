159c6794c1bb9980a77c64ed74c032a4be782143
Helmianto <helmianto15@gmail.com>
2021-07-03 22:43:50 +0700

    initial commit

---
+++ src/Page/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <center>
+                <h5>Home | Product | Kontak | Tentang Kami</h5>
+            </center>
+        </div>
+    );
+}
+
+export default Header;
